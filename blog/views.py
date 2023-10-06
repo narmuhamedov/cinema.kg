f0c36c2c23af3651a3017b09f167879e317c418a
rzpy1997 <kingradomir@gmail.com>
2023-10-06 21:14:55 +0600

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from . import models
+
+def post_view(request):
+    post_value = models.Post.objects.all()
+    return render(request, 'post.html', {'post_key':post_value})
