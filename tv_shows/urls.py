7682f405fcdb87e8ec06f40ac297f0863c8af47c
rzpy1997 <kingradomir@gmail.com>
2023-10-24 20:52:31 +0600

    'max_crud'

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.tvShowListView),
     path('show_detaill/<int:id>/', views.tvShowDetailView),
+    path('delete_list/', views.deleteListView),
+    path('update_list/', views.updateListView),
+    path('update_list/<int:id>/update/', views.editFilmView),
+    path('delete_list/<int:id>/delete/', views.dropFilmView),
+    path('create_film/', views.createShowView ),
 ]
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,14 +1,70 @@
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
+from django.http import HttpResponse
+
+def createShowView(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.TvShowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Успешно добавлен <a href="/"> На главную </a> ')
+    else:
+        form = forms.TvShowForm()
+
+    return render(request, 'crud/create_film.html', {'form': form})
+
+
+def deleteListView(request):
+    if request.method == 'GET':
+        show = models.TvShow.objects.all()
+        context = {
+            'show_key': show,
+        }
+        html_name = 'crud/del_film_list.html'
+        return render(request, html_name, context)
+
+
+def updateListView(request):
+    if request.method == 'GET':
+        show = models.TvShow.objects.all()
+        context = {
+            'show_key': show,
+        }
+        html_name = 'crud/update_film_list.html'
+        return render(request, html_name, context)
+
+def editFilmView(request, id):
+    film_id = get_object_or_404(models.TvShow, id=id)
+    if request.method == 'POST':
+        form = forms.TvShowForm(instance=film_id, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Успешно изменен <a href="/"> На главную </a> ')
+    else:
+        form = forms.TvShowForm(instance=film_id)
+    context = {
+        'film_key':film_id,
+        'form':form,
+    }
+    return render(request, 'crud/edit.html', context)
+
+
+def dropFilmView(request, id):
+    film_id = get_object_or_404(models.TvShow, id=id)
+    film_id.delete()
+    return HttpResponse('Успешно удален <a href="/"> На главную </a> ')
+
 
 
 def tvShowListView(request):
-    show = models.TvShow.objects.all()
-    context = {
-        'show_key': show,
-    }
-    html_name = 'tvshow/tvshow.html'
-    return render(request, html_name, context)
+    if request.method == 'GET':
+        show = models.TvShow.objects.all()
+        context = {
+            'show_key': show,
+        }
+        html_name = 'tvshow/tvshow.html'
+        return render(request, html_name, context)
 
 
 def tvShowDetailView(request, id):
